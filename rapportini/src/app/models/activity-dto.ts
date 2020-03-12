b42359566bd45da5113d0e58e981b7c0db69934f
dev-prime99 <prime99@icloud.com>
2020-03-12 15:39:18 +0100

    interpolazione dati (anagrafica, progetti, attività)

--- rapportini/src/app/models/activity-dto.ts
+++ rapportini/src/app/models/activity-dto.ts
@@ -1,10 +1,12 @@
 import { AnagraficaDto } from "./anagrafica-dto";
 import { TypeActivityDto } from "./type_activity-dto";
+import { ProjectDto } from "./project-dto";
 
 export interface ActivityDto {
     identity: string;
     customer: AnagraficaDto;
     activity: TypeActivityDto;
+    project: ProjectDto;
     date: Date;
     hours: number;
     desc: string;
--- rapportini/src/app/models/modules/activity/activity.component.ts
+++ rapportini/src/app/models/modules/activity/activity.component.ts
@@ -2,7 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router, ActivatedRoute } from '@angular/router';
 import { ActivityService } from 'src/app/service/activity.service';
+import { AnagraficaService } from 'src/app/service/anagrafica.service';
 import { mapDataFromModel } from 'src/app/models/functions/functions';
+import { AnagraficaDto } from 'src/app/models/anagrafica-dto';
+import { TypeActivityService } from 'src/app/service/type-activity.service';
+import { TypeActivityDto } from 'src/app/models/type_activity-dto';
+import { ProjectService } from 'src/app/service/project.service';
+import { ProjectDto } from 'src/app/models/project-dto';
 
 
 @Component({
@@ -14,12 +20,21 @@ export class ActivityComponent implements OnInit {
 
   activityForm: FormGroup;
   identity: string;
+  rows: AnagraficaDto[];
+  cars: TypeActivityDto[];
+  eyes: ProjectDto[];
 
-  constructor(private formBuilder: FormBuilder, private route:ActivatedRoute, private router:Router, private actService:ActivityService) {
+  constructor(private formBuilder: FormBuilder, private route:ActivatedRoute,
+    private router:Router,
+    private actService:ActivityService,
+    private anaService: AnagraficaService,
+    private typeAcService: TypeActivityService,
+    private prjService: ProjectService) {
     this.activityForm = formBuilder.group({
       identity: [null],
       customer: [null,Validators.required],
       activity: [null,Validators.required],
+      project: [null, Validators.required],
       date: [null,Validators.required],
       hours: [null,Validators.required],
       desc: [null]
@@ -36,7 +51,16 @@ export class ActivityComponent implements OnInit {
           this.activityForm.setValue(m);
         }
       })
-    }
+    };
+    this.anaService.getAnagraficaList().subscribe(x=>{
+      this.rows = x;
+    });
+    this.typeAcService.getTypeList().subscribe(x=>{
+      this.cars = x;
+    });
+    this.prjService.getProjectList().subscribe(x=>{
+      this.eyes = x;
+    });
   }
 
   toIndietro(){
